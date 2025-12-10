28822542b34f7030d5dbf20f59da77d235d9dd42
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2025-12-10 11:21:16 +0100

    file uplode

--- controllers/trainings.controller.js
+++ controllers/trainings.controller.js
@@ -1,4 +1,5 @@
 const Training = require('../models/Training')
+const path = require('path')
 
 exports.createTraining = async (req, res, next) => {
     try {
@@ -29,7 +30,7 @@ exports.getAllTraining = async (req, res, next) => {
             const sortBy = req.query.sort.split(',').join(' ')
             query = query.sort(sortBy)
         } else {
-            query = query.sort('-createdAt')  // Dátum szerint csökkenő rendezés
+            query = query.sort('-createdAt') // Dátum szerint csökkenő rendezés
         }
         query = query.skip(startIndex).limit(limit)
         const pagination = {};
@@ -59,6 +60,40 @@ exports.getTraining = async (req, res, next) => {
             return res.status(400).json({ success: false, msg: 'Not found' });
         }
         res.status(200).json({ success: true, data: training });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
+};
+
+exports.trainingPhotoUpload = async (req, res, next) => {
+    try {
+        const training = await Training.findById(req.params.id);
+        if (!training) {
+            return res.status(400).json({ success: false, msg: "Not found" });
+        }
+        if (!req.files) {
+            return res.status(400).json({ success: false, msg: "Please upload a file" });
+        }
+        const file = req.files.file
+        if (!file.mimetype.startsWith('image')) {
+            return res.status(400).json({ success: false, msg: "Please upload an image file" });
+        }
+        if (file.size > process.env.MAX_FILE_UPLOAD) {
+            return res.status(400).json({ success: false, msg: `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}` });
+        }
+
+        file.name = `photo_${training.id}${path.parse(file.name).ext}`
+        file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ success: false, msg: `Problem with file upload` });
+            }
+        })
+        await Training.findByIdAndUpdate(req.params.id, { photo: file.name })
+        res.status(200).json({
+            success: true,
+            data: file.name
+        })
     } catch (error) {
         res.status(400).json({ success: false });
     }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const qs = require("qs");
 require("dotenv").config(); // A .env fájlt olvassa
 const trainingsRoutes = require('./routes/trainings.routes')
@@ -16,7 +17,10 @@ database.once("connected", () => {
 const app = express()
 app.set('query parser', (str) => qs.parse(str));
 app.use(express.json())
-app.use('/api', trainingsRoutes)
+app.use(fileUpload())
+app.use(express.static('public'))
+app.use('/api/trainings', trainingsRoutes)
+app.use("/api/courses", coursesRoutes);
 
 app.listen(8000, ()=>{
     console.log('Server started on port 8000')
